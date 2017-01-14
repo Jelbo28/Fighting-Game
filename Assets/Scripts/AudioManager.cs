22b65116939210ec2f28626c77c7dd8d1ccffc8f
Aram Joseph <ajoseph@utc4me.org>
2017-01-13 20:21:20 -0500

    Audio!

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public List<AudioClip> audioClips;
+
+    AudioSource AS;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        AS = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound(int clipNumber)
+    {
+        AS.Stop();
+        if (name == "Songs")
+        {
+            AS.volume = 
+        }
+        AS.clip = audioClips[clipNumber];
+        AS.Play();
+    }
+
+}
---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PauseGame : MonoBehaviour
+{
+    public Transform canvas;
+    public Transform Player;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+    public void Pause()
+    {
+       
+        {
+            if (canvas.gameObject.activeInHierarchy == false)
+            {
+                canvas.gameObject.SetActive(true);
+                Time.timeScale = 0;
+            }
+            else
+            {
+                canvas.gameObject.SetActive(false);
+                Time.timeScale = 1;
+            }
+        }
+    }
+    public void ExitMainMenu()
+    {
+        Application.LoadLevel("MainMenu");
+    }
+}
